a6d54f383acc5fbdeea4f9832b6081e286738c88
bhrud <bhrud@AUTO1ZPWCTQVY7D>
2023-09-29 14:16:11 +0530

    Add project files.

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GildedRoseKata
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public static string AgedBrie = "Aged Brie";
+        public static string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+        public static string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public static string Conjured = "Conjured";
+        public static int MaximumQuality = 50;
+        public GildedRose(IList<Item> Items) => this.Items = Items;
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+            {
+                UpdateRegular(item);
+                UpdateBackStage(item);
+                UpdateAgedBrie(item);
+                UpdateConjured(item);
+                UpdateSulfuras(item);
+
+            }
+        }
+
+        private void UpdateSulfuras(Item item)
+        {
+        }
+
+        private void UpdateConjured(Item item)
+        {
+            if (IsConjured(item))
+                EditSellInandQuality(item, -2);
+        }
+        private void UpdateAgedBrie(Item item)
+        {
+            if (IsAgedBrie(item))
+                EditSellInandQuality(item, 1);
+        }
+        private void UpdateBackStage(Item item)
+        {
+            if (IsBackStage(item))
+                EditSellInandQuality(item, GetValueForBack(item));
+        }
+        private void UpdateRegular(Item item)
+        {
+            if (IsRegular(item))
+                EditSellInandQuality(item, -1);
+        }
+        private void EditSellInandQuality(Item item, int qualityValue)
+        {
+            item.SellIn--;
+            if (qualityValue == 0)
+                item.Quality = 0;
+            item.Quality += qualityValue;
+            if (item.SellIn < 0)
+                item.Quality += qualityValue;
+            item.Quality = Math.Clamp(item.Quality, 0, MaximumQuality);
+        }
+        private int GetValueForBack(Item item)
+        {
+            if (item.SellIn <= 0) return 0;
+            if (item.SellIn < 6) return 3;
+            if (item.SellIn < 11) return 2;
+            return 1;
+        }
+        private static bool IsConjured(Item item) => item.Name == Conjured;
+        private static bool IsRegular(Item item) =>
+            !(IsAgedBrie(item) || IsBackStage(item) || IsSulfuras(item) || IsConjured(item));
+        private static bool IsSulfuras(Item item) => item.Name == Sulfuras;
+        private static bool IsBackStage(Item item) => item.Name == BackstagePass;
+        private static bool IsAgedBrie(Item item) => item.Name == AgedBrie;
+    }
+}
---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+using static GildedRoseKata.GildedRose;
+
+namespace GildedRoseTests;
+public class GildedRoseTest
+{
+    private Item CreateAndUpdate(int sellIn, int quality, string itemName)
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = itemName, SellIn = sellIn, Quality = quality } };
+        GildedRose app = new(Items);
+        app.UpdateQuality();
+        return Items[0];
+    }
+    [Fact]
+    public void foo()
+    {
+        Item item = CreateAndUpdate(0, 0, "foo");
+        Assert.Equal("foo", item.Name);
+    }
+    [Fact]
+    public void SystemLowersValues()
+    {
+        Item item = CreateAndUpdate(15, 25, "foo");
+        Assert.Equal(14, item.SellIn);
+        Assert.Equal(24, item.Quality);
+    }
+
+    [Fact]
+    public void Qdegrees()
+    {
+        Item item = CreateAndUpdate(0, 17, "foo");
+        Assert.Equal(15, item.Quality);
+    }
+    [Fact]
+    public void NeverNegative()
+    {
+        Item item = CreateAndUpdate(0, 0, "foo");
+        Assert.Equal(0, item.Quality);
+    }
+    [Fact]
+    public void AgedBrieIncreases()
+    {
+        Item item = CreateAndUpdate(15, 25, AgedBrie);
+        Assert.Equal(26, item.Quality);
+    }
+    [Fact]
+    public void NeverExpire()
+    {
+        Item item = CreateAndUpdate(0, 42, AgedBrie);
+        Assert.Equal(-1, item.SellIn);
+        Assert.Equal(44, item.Quality);
+    }
+    [Fact]
+    public void BackstagePasses()
+    {
+        Item item = CreateAndUpdate(10, 48, BackstagePass);
+        Assert.Equal(MaximumQuality, item.Quality);
+        item = CreateAndUpdate(5, 49, BackstagePass);
+        Assert.Equal(MaximumQuality, item.Quality);
+        item = CreateAndUpdate(4, 30, BackstagePass);
+        Assert.Equal(33, item.Quality);
+        item = CreateAndUpdate(0, 30, BackstagePass);
+        Assert.Equal(0, item.Quality);
+        item = CreateAndUpdate(0, 30, BackstagePass);
+        Assert.Equal(0, item.Quality);
+    }
+
+    [Fact]
+    public void ConjuredItem()
+    {
+        Item item = CreateAndUpdate(15, 25, Conjured);
+        Assert.Equal(23, item.Quality);
+    }
+    
+}
